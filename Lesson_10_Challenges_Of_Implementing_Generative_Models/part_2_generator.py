a24c22bc61e04d1491e9c3688676e851e1e1e17b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-24 22:47:59 -0500

    Add Lesson_10_Challenges_Of_Implementing_Generative_Models/part_2_generator.py

---
+++ Lesson_10_Challenges_Of_Implementing_Generative_Models/part_2_generator.py
@@ -0,0 +1,18 @@
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(100, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        return self.model(z).view(-1, 1, 28, 28)
