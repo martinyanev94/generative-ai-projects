1fa0828fb1bff6aa6008d1257ec37e7543daadd2
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-24 22:50:19 -0500

    Add Lesson_29_Advanced_Applications_Of_Generative_Models_In_Art/part_4_data_augmentations.py

---
+++ Lesson_29_Advanced_Applications_Of_Generative_Models_In_Art/part_4_data_augmentations.py
@@ -0,0 +1,8 @@
+from torchvision import transforms
+
+data_transforms = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.ToTensor(),
+])
