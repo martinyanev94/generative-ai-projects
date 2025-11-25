7c09593da6793f18e0c640ad1484fbc955f97f51
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-24 22:50:27 -0500

    Add Lesson_2_Applications_Of_Deep_Learning_In_Generative_Modeling/part_4_variational_autoencoder.py

---
+++ Lesson_2_Applications_Of_Deep_Learning_In_Generative_Modeling/part_4_variational_autoencoder.py
@@ -0,0 +1,15 @@
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        epsilon = torch.randn_like(std)
+        return mu + epsilon * std
+
+    def forward(self, x):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decoder(z), mu, logvar
