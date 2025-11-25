1fa45b44949bd940e1575f3cdccc149357a83bf9
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-24 22:49:32 -0500

    Add Lesson_23_Implementation_Of_Generative_Models_In_Pytorch/part_1_torch_ml_visualization.bash

---
+++ Lesson_23_Implementation_Of_Generative_Models_In_Pytorch/part_1_torch_ml_visualization.bash
@@ -0,0 +1,7 @@
+pip install torch torchvision matplotlib
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision import datasets
+import matplotlib.pyplot as plt
