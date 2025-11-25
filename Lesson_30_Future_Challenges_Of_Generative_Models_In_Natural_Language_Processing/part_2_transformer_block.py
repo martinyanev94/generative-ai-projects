4e3221fe3db831c359f3f3794ee093127bb288ee
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-24 22:50:34 -0500

    Add Lesson_30_Future_Challenges_Of_Generative_Models_In_Natural_Language_Processing/part_2_transformer_block.py

---
+++ Lesson_30_Future_Challenges_Of_Generative_Models_In_Natural_Language_Processing/part_2_transformer_block.py
@@ -0,0 +1,19 @@
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = nn.MultiheadAttention(embed_dim=embed_size, num_heads=heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size)
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        attention = self.attention(x, x, x)[0]
+        x = self.dropout(self.norm1(attention + x))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
