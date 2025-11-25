def86dadfd980f18215cd1bd5452478ce9421e65
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-24 22:49:56 -0500

    Add Lesson_26_Advancements_In_Generative_Models_Across_Industries/part_3_extract_features.py

---
+++ Lesson_26_Advancements_In_Generative_Models_Across_Industries/part_3_extract_features.py
@@ -0,0 +1,16 @@
+def get_features(image, model):
+    layers = {
+        '0': 'conv1_1',
+        '5': 'conv2_1',
+        '10': 'conv3_1',
+        '19': 'conv4_1',
+        '21': 'conv4_2',  # Content layer
+        '28': 'conv5_1'
+    }
+    features = {}
+    x = image
+    for name, layer in model._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+    return features
